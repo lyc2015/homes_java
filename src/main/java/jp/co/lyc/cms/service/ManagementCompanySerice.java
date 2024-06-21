b00afc33899bc179f82f32e9586bf4ea96b482af
tianyuanyyy <109130123+tianyuanyyy@users.noreply.github.com>
2024-06-21 18:49:09 +0900

    Update ManagementCompanySerice.java

--- src/main/java/jp/co/lyc/cms/service/ManagementCompanySerice.java
+++ src/main/java/jp/co/lyc/cms/service/ManagementCompanySerice.java
@@ -1,5 +1,7 @@
 package jp.co.lyc.cms.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,7 +30,7 @@ public class ManagementCompanySerice {
     @Transactional(rollbackFor = Exception.class)
     public String searchMaxManagementCompanyID() {
         try {
-           String maxNo = managementCompanyMapper.searchMaxManagementCompanyID();
+            String maxNo = managementCompanyMapper.searchMaxManagementCompanyID();
             // 可以根据需要添加更多的插入逻辑
             return maxNo;
         } catch (Exception e) {
@@ -36,4 +38,57 @@ public class ManagementCompanySerice {
             return null;
         }
     }
+
+    @Transactional(rollbackFor = Exception.class)
+    public List<ManagementCompanyModel> searchAllManagementCompanyInfo() {
+        try {
+            List<ManagementCompanyModel> getAllInfo = managementCompanyMapper.searchAllManagementCompanyInfo();
+            // 可以根据需要添加更多的插入逻辑
+            return getAllInfo;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // @Transactional(rollbackFor = Exception.class)
+    // public ManagementCompanyModel getManagementCompanyInfoByID(ManagementCompanyModel managementCompanyModel) {
+    //     try {
+    //         ManagementCompanyModel getInfo = managementCompanyMapper.getManagementCompanyInfoByID(managementCompanyModel);
+    //         // 会社IDで会社情報を取得
+    //         return getInfo;
+    //     } catch (Exception e) {
+    //         e.printStackTrace();
+    //         return null;
+    //     }
+    // }
+
+
+    @Transactional(rollbackFor = Exception.class)
+    public List<ManagementCompanyModel> getManagementCompanyInfoByID(ManagementCompanyModel managementCompanyModel) {
+        try {
+            List<ManagementCompanyModel> getInfo = managementCompanyMapper.getManagementCompanyInfoByID(managementCompanyModel);
+            // 会社IDで会社情報を取得
+            return getInfo;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
+    @Transactional(rollbackFor = Exception.class)
+    public int updateCompanyInfo(ManagementCompanyModel managementCompanyModel) {
+        try {
+            int updateResult = managementCompanyMapper.updateManagementCompanyInfo(managementCompanyModel);
+            // 可以根据需要添加更多的插入逻辑
+            return updateResult;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+
 }
