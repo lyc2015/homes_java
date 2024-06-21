a9d6092394827b3376b3df93b98e5a16d2ce74eb
tianyuanyyy <109130123+tianyuanyyy@users.noreply.github.com>
2024-06-21 18:48:15 +0900

    Update ManagementCompanyMapper.java

--- src/main/java/jp/co/lyc/cms/mapper/ManagementCompanyMapper.java
+++ src/main/java/jp/co/lyc/cms/mapper/ManagementCompanyMapper.java
@@ -1,20 +1,25 @@
 package jp.co.lyc.cms.mapper;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
-
 import jp.co.lyc.cms.model.ManagementCompanyModel;
 
 @Mapper
 public interface ManagementCompanyMapper {
-
     // 管理会社を登録
     public void registerManagementCompany(ManagementCompanyModel managementCompanyModel);
 
     // 管理会社最大IDを取得
     public String searchMaxManagementCompanyID();
+
+    // すべての管理会社IDと会社名を取得
+    public List<ManagementCompanyModel> searchAllManagementCompanyInfo();
+
+    // 管理会社IDで、会社情報を取得
+    public List<ManagementCompanyModel> getManagementCompanyInfoByID(ManagementCompanyModel managementCompanyModel);
+
+    //管理会社情報更新
+    public int updateManagementCompanyInfo(ManagementCompanyModel managementCompanyModel);
 
 }
